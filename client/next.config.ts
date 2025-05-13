8f7679c1eedb52d08a90fcf4c0e0d46d25d4b18d
Bhavesh <bhavesh@jklu.edu.in>
2025-05-13 13:04:27 +0530

    q

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from "next";
+import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin";
 
 const nextConfig: NextConfig = {
   images: {
@@ -21,6 +22,12 @@ const nextConfig: NextConfig = {
     },
   },
   output: "standalone",
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...(config.plugins || []), new PrismaPlugin()];
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
