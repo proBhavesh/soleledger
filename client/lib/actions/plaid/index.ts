4ee92a2ea8cf25295862524ecbffb6472b282490
Bhavesh <bhavesh@jklu.edu.in>
2025-05-15 17:00:10 +0530

    q

--- client/lib/actions/plaid/index.ts
+++ client/lib/actions/plaid/index.ts
@@ -1,6 +1,5 @@
-"use server";
-
 // Re-export all actions from the different files
+// Each imported file already has its own "use server" directive
 export { createLinkToken, exchangePublicToken } from "./auth";
 export { getBankAccounts } from "./accounts";
 export { getFinancialSummary } from "./summary";
